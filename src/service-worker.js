b4036b929791b9e60df8453a6cd196f8f15db23e
nbabcock <nathan.r.babcock@gmail.com>
2023-08-11 15:45:37 -0500

    feat: migrate to chrome extension manifest v3

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,9 +7,9 @@ chrome.contextMenus.create({
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "create-gcal-url") {
     chrome.storage.sync.get(["apiKey"], result => {
-      chrome.tabs.insertCSS({
-        code: 'body { cursor: wait; }'
-      })
+      // chrome.tabs.insertCSS({
+      //   code: 'body { cursor: wait; }'
+      // })
       if (result.apiKey === undefined) {
         alert("API key is not set. Please set it in the extension options.")
         chrome.runtime.openOptionsPage()
@@ -62,11 +62,52 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
           alert(error.message)
           console.error(error)
         } finally {
-          chrome.tabs.insertCSS({
-            code: 'body { cursor: default; }'
-          })
+          // chrome.tabs.insertCSS({
+          //   code: 'body { cursor: default; }'
+          // })
         }
       }
     })
   }
 })
+
+// 🔥 HOT RELOADING 🔥
+// https://github.com/xpl/crx-hotreload
+// todo: migrate to https://github.com/pacexy/mv3-hot-reload ?
+
+// const filesInDirectory = dir => new Promise(resolve =>
+//   dir.createReader().readEntries(entries =>
+//     Promise.all(entries.filter(e => e.name[0] !== '.').map(e =>
+//       e.isDirectory
+//         ? filesInDirectory(e)
+//         : new Promise(resolve => e.file(resolve))
+//     ))
+//       .then(files => [].concat(...files))
+//       .then(resolve)
+//   )
+// )
+
+// const timestampForFilesInDirectory = dir =>
+//   filesInDirectory(dir).then(files =>
+//     files.map(f => f.name + f.lastModifiedDate).join())
+
+// const watchChanges = (dir, lastTimestamp) => {
+//   timestampForFilesInDirectory(dir).then(timestamp => {
+//     if (!lastTimestamp || (lastTimestamp === timestamp)) {
+//       setTimeout(() => watchChanges(dir, timestamp), 1000) // retry after 1s
+//     } else {
+//       chrome.runtime.reload()
+//     }
+//   })
+// }
+
+// chrome.management.getSelf(self => {
+//   if (self.installType === 'development') {
+//     chrome.runtime.getPackageDirectoryEntry(dir => watchChanges(dir))
+//     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => { // NB: see https://github.com/xpl/crx-hotreload/issues/5
+//       if (tabs[0]) {
+//         chrome.tabs.reload(tabs[0].id)
+//       }
+//     })
+//   }
+// })
