384159139cef519652ef82d6809459cbcccb0504
nbabcock <nathan.r.babcock@gmail.com>
2023-08-11 17:20:52 -0500

    refactor: add early exits

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,82 +1,82 @@
 console.log('Add To Calendar service worker started')
 
 chrome.contextMenus.create({
-  id: "create-gcal-url",
-  title: "Create Google Calendar event",
-  contexts: ["selection"]
+  id: 'create-gcal-url',
+  title: 'Create Google Calendar event',
+  contexts: ['selection']
 })
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "create-gcal-url") {
-    chrome.storage.sync.get(["apiKey"], result => {
-      chrome.scripting.insertCSS({
-        target: { tabId: tab.id },
-        css: 'body { cursor: wait; }'
-      })
-      if (result.apiKey === undefined) {
-        alert("API key is not set. Please set it in the extension options.")
-        chrome.runtime.openOptionsPage()
-      } else {
-        console.log({ selectionText: info.selectionText })
+  if (info.menuItemId !== 'create-gcal-url') return
+  chrome.storage.sync.get(['apiKey'], result => {
+    chrome.scripting.insertCSS({
+      target: { tabId: tab.id },
+      css: 'body { cursor: wait; }'
+    })
 
-        const apiKey = result.apiKey
-        const endpoint = "https://api.openai.com/v1/chat/completions"
+    if (result.apiKey === undefined) {
+      alert('API key is not set. Please set it in the extension options.')
+      chrome.runtime.openOptionsPage()
+      return
+    }
 
-        const now = new Date()
-        const localTime = now.toLocaleTimeString()
-        const localDate = now.toLocaleDateString()
+    console.log({ selectionText: info.selectionText })
 
-        const model = "gpt-3.5-turbo-0613"
-        const prompt = 'Create an "add to google calendar" link '
-          + 'based on the following description:\n'
-          + '- Respond with just the link by itself, as a raw URL.\n'
-          + `- Take into account the user's local time is ${localTime} on ${localDate}.\n`
-          + '- Include the location of the event if it is provided.\n'
-          + '- Include the full description text given in the description of the event.\n'
-          + '- Add an appropriate emoji at the beginning of the title.\n'
-          + 'The event description is as follows:\n'
-          + '\n'
-          + info.selectionText
+    const apiKey = result.apiKey
+    const endpoint = 'https://api.openai.com/v1/chat/completions'
 
-        const messages = [
-          { role: 'user', content: prompt }
-        ]
+    const now = new Date()
+    const localTime = now.toLocaleTimeString()
+    const localDate = now.toLocaleDateString()
 
-        console.log({ prompt })
+    const model = 'gpt-3.5-turbo-0613'
+    const prompt = 'Create an "add to google calendar" link '
+      + 'based on the following description:\n'
+      + '- Respond with just the link by itself, as a raw URL.\n'
+      + `- Take into account the user's local time is ${localTime} on ${localDate}.\n`
+      + '- Include the location of the event if it is provided.\n'
+      + '- Include the full description text given in the description of the event.\n'
+      + '- Add an appropriate emoji at the beginning of the title.\n'
+      + 'The event description is as follows:\n'
+      + '\n'
+      + info.selectionText
 
-        try {
-          fetch(endpoint, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "Authorization": `Bearer ${apiKey}`
-            },
-            body: JSON.stringify({
-              model,
-              messages,
-            })
-          })
-            .then(response => response.json())
-            .then(data => {
-              const url = data.choices[0].message.content
-              console.log({ url })
-              if (!url) return alert('Could not parse event from text.')
-              if (!url.startsWith('http')) return alert(url)
-              chrome.tabs.create({ url })
-              chrome.scripting.insertCSS({
-                target: { tabId: tab.id },
-                css: 'body { cursor: unset; }'
-              })
-            })
-        } catch (error) {
-          alert(error.message)
-          console.error(error)
+    const messages = [
+      { role: 'user', content: prompt }
+    ]
+
+    console.log({ prompt })
+
+    try {
+      fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`
+        },
+        body: JSON.stringify({
+          model,
+          messages,
+        })
+      }).then(response => response.json())
+        .then(data => {
+          const url = data.choices[0].message.content
+          console.log({ url })
+          if (!url) return alert('Could not parse event from text.')
+          if (!url.startsWith('http')) return alert(url)
+          chrome.tabs.create({ url })
           chrome.scripting.insertCSS({
             target: { tabId: tab.id },
             css: 'body { cursor: unset; }'
           })
-        }
-      }
-    })
-  }
+        })
+    } catch (error) {
+      alert(error.message)
+      console.error(error)
+      chrome.scripting.insertCSS({
+        target: { tabId: tab.id },
+        css: 'body { cursor: unset; }'
+      })
+    }
+  })
 })
