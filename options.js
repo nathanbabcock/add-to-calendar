4d425e5d5a171b8332a5040d214c0d9fb130f58c
nbabcock <nathan.r.babcock@gmail.com>
2023-08-11 14:24:51 -0500

    refactor: move code into src

--- options.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("api-key-form").addEventListener("submit", function (event) {
-  event.preventDefault()
-  const apiKey = document.getElementById("api-key-input").value
-  chrome.storage.sync.set({ apiKey: apiKey }, () => { alert("💾 Saved") })
-})
